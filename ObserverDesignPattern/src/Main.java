24b6f7de65bf613c71f350f5b2931f7222ece8bc
jordangeorgiev89 <jordangeorgiev89@gmail.com>
2020-06-04 17:30:39 +0300

    update

---
+++ ObserverDesignPattern/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        StockGrabber stockGrabber = new StockGrabber();
+        StockObserver observer1 = new StockObserver(stockGrabber);
+
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+
+        StockObserver observer2 = new StockObserver(stockGrabber);
+
+        stockGrabber.unregister(observer1);
+
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+
+        Runnable getIBM = new GetTheStock(stockGrabber, 2, "IBM", 197.00);
+        Runnable getAAPL = new GetTheStock(stockGrabber, 2, "IBM", 677.60);
+        Runnable getGOOG = new GetTheStock(stockGrabber, 2, "IBM", 676.40);
+
+        new Thread(getIBM).start();
+        new Thread(getAAPL).start();
+        new Thread(getGOOG).start();
+    }
+}
